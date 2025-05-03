0b0e0b415ec7d4376e49d2989d2676e4f2c72956
Wez237 <wezlei.filho@a.ucb.br>
2025-05-03 14:20:13 -0300

    Create ED1Q1.c

---
+++ ED1Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int BinToDec(const char *bin) {
+    int dec = 0;
+    while (*bin) {
+        dec = dec * 2 + (*bin++ - '0');
+    } return dec;
+} int main() {
+    const char *bins[] = {"100101", "1000101101", "1111010110110"};
+    int size = sizeof(bins) / sizeof(bins[0]);
+
+    printf("Conversão de binário para decimal e hexadecimal:\n");
+
+    for (int i = 0; i < size; i++) {
+        int dec = BinToDec(bins[i]);
+
+        printf("Binario: %s\n", bins[i]);
+        printf("Decimal: %d\n", dec);
+        printf("Hexadecimal: %X\n\n", dec);
+    } return 0;
+}
